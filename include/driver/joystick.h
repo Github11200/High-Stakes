999540e70b145c58da2ec86ea20d369e75fefe9a
Github11200 <78944617+Github11200@users.noreply.github.com>
2024-09-18 15:58:28 -0700

    working on the driver control

---
+++ include/driver/joystick.h
@@ -0,0 +1,19 @@
+#ifndef JOYSTICK_H
+#define JOYSTICK_H
+
+#include "vex.h"
+
+using namespace vex;
+using namespace std;
+
+class Joystick
+{
+private:
+  double calculateBezierOutput(double power, double turn)
+  {
+  }
+
+public:
+};
+
+#endif
