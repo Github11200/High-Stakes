145c022030064e36d04f0c97d02a1db6c335800d
Github11200 <78944617+Github11200@users.noreply.github.com>
2025-04-18 11:59:21 -0700

    tuning the timeouts and stuff

--- src/autons.cpp
+++ src/autons.cpp
@@ -138,62 +138,63 @@ void Autonomous::solo_awp()
                                      { ladyBrown.autonLoading(); cout << "brought it back..." << endl; this_thread::yield(); });
 
   // Turn towards the mogo
-  chassis.turn_to_point(-20.099, 16.6367, 180, this->turnParams.set_timeout(500));
+  chassis.turn_to_point(-20.099, 16.6367, 180, this->turnParams.set_timeout(900));
 
-  // Curve into goal and clamp
-  mogoClampDelay = 1000;
+  // Move into goal and clamp
+  mogoClampDelay = 900;
   mogoClamp = true;
 
   // Drive into the mogo and clamp
-  chassis.drive_to_point(-20.099, 16.6367, DriveParams().set_min_voltage(2).set_timeout(600).set_settle_time(0).set_settle_error(0.5));
+  chassis.drive_to_point(-20.099, 16.6367, DriveParams().set_min_voltage(3).set_timeout(1000).set_settle_time(0).set_settle_error(0.1));
 
   intakeSort = true;
 
   // Go to the rings in the middle
-  chassis.turn_to_point(-7.47664, 32.7364, 0, this->turnParams.set_timeout(200));
+  chassis.turn_to_point(-7.47664, 32.7364, 0, this->turnParams.set_timeout(1000));
   LeftDoinker.set(true);
   DriveParams temporaryDriveWithMogoParams = this->driveParamsWithMogo;
-  chassis.drive_to_point(-7.47664, 32.7364, temporaryDriveWithMogoParams.set_timeout(800));
+  chassis.drive_to_point(-7.47664, 32.7364, temporaryDriveWithMogoParams.set_timeout(1200));
 
   // Pull the ring back with the doinker
   temporaryDriveWithMogoParams = this->driveParamsWithMogo;
-  chassis.turn_to_point(-33.1289, 0.491801, 180, this->turnParams.set_timeout(100).set_settle_time(0).set_settle_error(1));
-  chassis.drive_to_point(-33.1289, 0.491801, temporaryDriveWithMogoParams.set_timeout(800).set_settle_time(0).set_settle_error(0.5).set_min_voltage(2));
+  chassis.turn_to_point(-33.1289, 0.491801, 180, this->turnParams.set_timeout(800).set_settle_time(0).set_settle_error(1));
+  chassis.drive_to_point(-33.1289, 0.491801, temporaryDriveWithMogoParams.set_timeout(1200).set_settle_time(0).set_settle_error(0.1).set_min_voltage(2));
 
   LeftDoinker.set(false);
 
   // Score the other two rings which are in a line
   temporaryDriveWithMogoParams = this->driveParamsWithMogo;
-  chassis.turn_to_point(-25.1708, 41.9966, 0, this->turnParams.set_timeout(100));
-  chassis.drive_to_point(-25.1708, 41.9966, temporaryDriveWithMogoParams.set_timeout(1000).set_settle_time(0).set_settle_error(0.1).set_min_voltage(5));
+  chassis.turn_to_point(-25.1708, 41.9966, 0, this->turnParams.set_timeout(800));
+  chassis.drive_to_point(-25.1708, 41.9966, temporaryDriveWithMogoParams.set_timeout(1200).set_settle_time(0).set_settle_error(0.1).set_min_voltage(2));
 
   // Turn towards the point from which we align to the 2 stack of rings
-  chassis.turn_to_point(-49.9043, 15.9103, 0, this->turnParams.set_timeout(100));
+  chassis.turn_to_point(-49.6156, 15.9103, 0, this->turnParams.set_timeout(800));
   mogoClamp = false;
   Clamp.set(false);
-  chassis.drive_to_point(-49.9043, 15.9103, DriveParams().set_timeout(600).set_settle_time(0).set_settle_error(2).set_min_voltage(5));
+  wait(200, vex::timeUnits::msec);
+  chassis.drive_to_point(-49.6156, 15.9103, DriveParams().set_timeout(1500).set_settle_time(0).set_settle_error(0.1).set_min_voltage(2));
 
   // Move into the stack and intake it
-  chassis.turn_to_point(-49.6156, -27.4898, 0, this->turnParams.set_timeout(100));
+  chassis.turn_to_point(-49.6156, -27.4898, 0, this->turnParams.set_timeout(800));
   thread intakeStackThread = thread([]()
                                     {
                                       // We've already intaked the ring we don't want and are now intaking the one we do want
                                       if (!intakeControl.shouldEjectRing()) {
                                         intakeSort = false;
-                                        this_thread::yield();
+                                        this_thread::yield(); 
                                       }
                                       wait(20, vex::timeUnits::msec); });
-  chassis.drive_to_point(-49.6156, -27.4898, DriveParams().set_timeout(700).set_settle_time(0).set_settle_error(1).set_min_voltage(2));
+  chassis.drive_to_point(-49.6156, -27.4898, DriveParams().set_timeout(1000).set_settle_time(0).set_settle_error(0.1).set_min_voltage(2));
 
   // Turn towards the second mogo and drive into it
-  chassis.turn_to_point(-23.2287, -28.8152, 180, this->turnParams.set_timeout(400));
+  chassis.turn_to_point(-23.2287, -28.8152, 180, this->turnParams.set_timeout(800));
   mogoClampDelay = 1200;
   mogoClamp = true;
-  chassis.drive_to_point(-23.2287, -28.8152, DriveParams().set_timeout(800).set_settle_time(0).set_settle_error(0.5).set_min_voltage(2));
+  chassis.drive_to_point(-23.2287, -28.8152, DriveParams().set_timeout(1000).set_settle_time(0).set_settle_error(0.5).set_min_voltage(2));
   intakeSort = true;
 
-  LadyBrownMotor.spin(vex::directionType::fwd, 12, vex::voltageUnits::volt);
-  chassis.turn_to_point(-21.2926, -26.553, 0, this->turnParams.set_timeout(200));
+  LadyBrownMotor.spin(vex::directionType::fwd, 8, vex::voltageUnits::volt);
+  chassis.turn_to_point(-21.2926, -26.553, 0, this->turnParams.set_timeout(1500));
 }
 
 // NOT TESTED
